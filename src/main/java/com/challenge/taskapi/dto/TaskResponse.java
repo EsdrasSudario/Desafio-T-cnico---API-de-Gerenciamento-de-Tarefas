dff3ceabc08cdb75bb7560d30059b376d4e32273
Esdras Sudario <esdrassudario@gmail.com>
2025-10-22 20:07:35 -0300

    Feito correção para compilar projeto.

--- src/main/java/com/challenge/taskapi/dto/TaskResponse.java
+++ src/main/java/com/challenge/taskapi/dto/TaskResponse.java
@@ -1,15 +1,11 @@
 package com.challenge.taskapi.dto;
 
 import com.challenge.taskapi.enums.Priority;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class TaskResponse {
 
 	private Long id;
